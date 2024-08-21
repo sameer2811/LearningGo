83b83e5509337f9f99610de04f880e47465c9581
Sameer Singh <sameersingh@playsimple.in>
2024-08-21 20:47:45 +0530

    Adding another approach for the variables declaration

--- HelloWorld/investment_calculator.go
+++ HelloWorld/investment_calculator.go
@@ -6,10 +6,22 @@ import (
 )
 
 func main() {
-	var principalValue int = 1000
-	var rateOfIntrest float64 = 5.23
-	var timeInYears int = 5
 
-	var futureValue = float64(principalValue) * math.Pow((1+rateOfIntrest/100), float64(timeInYears))
+	/*
+		Approach 1
+		var principalValue int = 1000
+		var rateOfIntrest float64 = 5.23
+		var timeInYears int = 5
+
+		var futureValue = float64(principalValue) * math.Pow((1+rateOfIntrest/100), float64(timeInYears))
+		fmt.Println(futureValue)
+	*/
+
+	// Approach 2
+	principalValue := 1000.0
+	rateOfIntrest := 5.5
+	timeInYears := 2.0
+
+	futureValue := principalValue * math.Pow((1+rateOfIntrest/100), timeInYears)
 	fmt.Println(futureValue)
 }

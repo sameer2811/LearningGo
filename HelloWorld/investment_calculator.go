b367cf8ae642592adfa3ae75e7bffb5beddd29fc
Sameer Singh <sameersingh@playsimple.in>
2024-08-21 21:07:34 +0530

    some new things taught for the variables

--- HelloWorld/investment_calculator.go
+++ HelloWorld/investment_calculator.go
@@ -18,10 +18,27 @@ func main() {
 	*/
 
 	// Approach 2
+
+	// Declared and value assigned automatically
+	var inflationRate, price float64 = 30.0, 90 // Declaring multiple values
+	fmt.Println(inflationRate, price)
+
+	// Another way of declaring variables without specifying datatype
+	newVariablesDeclared, dsfdsfs := 23, 213 // compiler will infer the type accordingly
+
+	fmt.Println(newVariablesDeclared, dsfdsfs)
+
+	// value reassigned
+	inflationRate = 20.0
+
 	principalValue := 1000.0
 	rateOfIntrest := 5.5
 	timeInYears := 2.0
 
 	futureValue := principalValue * math.Pow((1+rateOfIntrest/100), timeInYears)
+	futureRealPrice := futureValue / (1 + math.Pow(1+inflationRate/100, timeInYears))
+
+	
 	fmt.Println(futureValue)
+	fmt.Println(futureRealPrice)
 }
